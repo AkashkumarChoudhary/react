93034a97ec22d7494297f67d5a05a2efeea478a4
AkashkumarChoudhary <akcmumbai13579@gmail.com>
2024-07-10 22:53:13 +0530

    10/07

--- app/page.js
+++ app/page.js
@@ -1,1 +1,17 @@
-import React, { useState, useEffect } from 'react';
+"use client"
+import React, { useState } from 'react'
+
+const page = () => {
+  const[marks, setMarks] = useState(50)
+  return (
+    <>
+    <h1>My marks were {marks}</h1>
+    <button onClick={()=> {
+      setMarks(56)
+    }} >Update</button>
+    </>
+  )
+}
+
+export default page
+
