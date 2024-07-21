e311d0bd4906121a950d72c97788845e3b33e6f1
AkashkumarChoudhary <akcmumbai13579@gmail.com>
2024-07-21 21:51:35 +0530

    21

--- app/page.js
+++ app/page.js
@@ -2,8 +2,10 @@
   const page = () => {
    return (
      <div>
-      <h2>This is Home page </h2>
-       
+      <h2>This is Home page  and headder
+        
+      </h2>
+       <h3>where R Y</h3>
 
      </div>
    )
